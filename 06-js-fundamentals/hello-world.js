abccaa3df7f72af35dbcd7c373c2af6d8b3fec66
Tiago Machado <tmxx37@gmail.com>
2020-12-26 09:45:31 +0000

    bootstrap and js fundamentals

---
+++ 06-js-fundamentals/hello-world.js
@@ -0,0 +1,22 @@
+/*comment*/
+
+/**JSDoc comment*/
+
+//inline comment
+
+/*alert("Hello world");
+alert("Hello"); 
+alert("World");
+alert( 3 + 1 + 2);*/
+
+//declaring a variable
+let message;
+
+//assign a value to the variable
+message = "Hello world!";
+
+//or mix them
+let message = "Hello world!";
+
+//referencing the variable
+alert(message);
