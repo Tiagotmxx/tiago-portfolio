edde73273d1b67eeca166d84ca2c2b00f90c2bbb
Tiago Machado <tmxx37@gmail.com>
2021-06-17 18:48:33 +0100

    modular reatc app

---
+++ todos-app/src/server-app/app.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import List from "./list";
+import Form from "./form";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const handleSubmit = (text) => {
+    const maxId = todos.length === 0 ? 0 : todos[todos.length - 1].id;
+    //const maxId = todos.length ? todos[todos.length - 1].id : 0;
+
+    setTodos([...todos, { id: maxId + 1, text, done: false }]);
+  };
+
+  const handleSpanClick = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+
+  const handleButtonClick = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} />
+      <List
+        todos={todos}
+        onSpanClick={handleSpanClick}
+        onButtonClick={handleButtonClick}
+      />
+    </>
+  );
+}
